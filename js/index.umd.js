a6a076da4ec020ab9882709051717e2653c827d7
Ivan Nikolov <ivan@xtremify.com>
2024-09-02 10:08:43 +0300

    Light and dark colors removed; danger color added back; documents update

--- js/index.umd.js
+++ js/index.umd.js
@@ -5,13 +5,13 @@
  * --------------------------------------------------------------------------
  */
 
-import Alert from './src/alert.js'
-import Modal from './src/modal.js'
 import Button from './src/button.js'
-/* import Carousel from './src/carousel.js'
-import Collapse from './src/collapse.js' */
+/* import Alert from './src/alert.js'
+import Modal from './src/modal.js'
+import Carousel from './src/carousel.js'
+import Collapse from './src/collapse.js'
 import Dropdown from './src/dropdown.js'
-/* import Offcanvas from './src/offcanvas.js'
+import Offcanvas from './src/offcanvas.js'
 import Popover from './src/popover.js'
 import ScrollSpy from './src/scrollspy.js'
 import Tab from './src/tab.js'
@@ -19,13 +19,13 @@ import Toast from './src/toast.js'
 import Tooltip from './src/tooltip.js' */
 
 export default {
-  Alert,
+  Button
+  /* Alert,
   Modal,
-  Button,
-  /* Carousel,
-  Collapse, */
-  Dropdown
-  /* Offcanvas,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Offcanvas,
   Popover,
   ScrollSpy,
   Tab,
